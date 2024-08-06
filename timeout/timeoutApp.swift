ed5bf210fe5ad422712f8c258de045eb394f86ad
Jack Noel <jnoeljo@jci.com>
2024-08-05 20:37:40 -0400

    timeout init commit

---
+++ timeout/timeoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeoutApp.swift
+//  timeout
+//
+//  Created by Jack Noel on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct timeoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
